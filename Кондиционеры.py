bd9d217c1ac0cf1d8702d485e71b5ff7c8c23d31
Alex_Ting <adr.tsiklov@mail.ru>
2021-11-20 14:46:59 +0300

    13 тест tl

--- Кондиционеры.py
+++ Кондиционеры.py
@@ -1,8 +1,20 @@
-print('uhukblk')
-for i in range(10):
-    print('fdzgfd')
-    print('dsvsfs')
-    print(i)
+class_count = int(input())
+class_requirements = list(map(int, input().split()))
+models_count = int(input())
+models_dict = {}
+for i in range(models_count):
+    power, price = map(int, input().split())
+    if power not in models_dict or models_dict[power] > price:
+        models_dict[power] = price
 
-def dd():
-    print('wgwdgewrg')
+summa = 0
+for i in class_requirements:
+    m = 10**10
+    price = 0
+    for j in models_dict:
+        if j >= i and models_dict[j] < m:
+            m = models_dict[j]
+            price = models_dict[j]
+    summa += price
+
+print(summa)
