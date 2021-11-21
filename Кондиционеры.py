1b9520e4884f727832f905c3cf708d6a1873811c
Alex_Ting <adr.tsiklov@mail.ru>
2021-11-21 13:22:05 +0300

    OK

--- Кондиционеры.py
+++ Кондиционеры.py
@@ -1,20 +1,31 @@
 class_count = int(input())
 class_requirements = list(map(int, input().split()))
 models_count = int(input())
-models_dict = {}
+models_dict, new_models_dict = {}, {}
 for i in range(models_count):
     power, price = map(int, input().split())
     if power not in models_dict or models_dict[power] > price:
         models_dict[power] = price
 
-summa = 0
-for i in class_requirements:
-    m = 10**10
-    price = 0
-    for j in models_dict:
-        if j >= i and models_dict[j] < m:
-            m = models_dict[j]
-            price = models_dict[j]
-    summa += price
+models_dict = dict(sorted(models_dict.items(), reverse=True))
+min_price = ''
+for power in models_dict:
+    if min_price == '':
+        min_price = models_dict[power]
+        new_models_dict[power] = models_dict[power]
 
-print(summa)
+    if models_dict[power] < min_price:
+        min_price = models_dict[power]
+        new_models_dict[power] = min_price
+
+total_price = 0
+new_models_dict = dict(sorted(new_models_dict.items()))
+for req in class_requirements:
+    ''' Чудом не ловит TL с циклом ниже, в идеале делать вместо него 2 указатель,
+    чтобы каждый раз не бегать по меньшим мощностям'''
+    for power in new_models_dict:
+        if power >= req:
+            total_price += new_models_dict[power]
+            break
+
+print(total_price)
