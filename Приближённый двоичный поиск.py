78ac66de56386bb7712634cea9bb7177ba426705
Alex_Ting <adr.tsiklov@mail.ru>
2021-11-26 12:26:34 +0300

    ловит RE

--- Приближённый двоичный поиск.py
+++ Приближённый двоичный поиск.py
@@ -1,17 +1,20 @@
-import math
-
 n, k = input().split()
 arr1 = list(map(int, input().split()))
 set1 = set(arr1)
 sorted_set = sorted(list(set1))
+len_set = len(sorted_set)
 arr2 = list(map(int, input().split()))
 
 
 def bin_search(arr, elem):
     length = len(arr)
     R, L = length, 0
-    while R != L:
+    while L < R:
         m = (R + L + 1) // 2
+
+        if m == length:
+            break
+
         if elem > arr[m]:
             L = m
         else:
@@ -23,7 +26,17 @@ def bin_search(arr, elem):
 
 
 for elem in arr2:
-    print(bin_search(arr1, elem))
+    ind = bin_search(sorted_set, elem)
+    dif1 = abs(sorted_set[ind] - elem)
+    try:
+        dif2 = abs(sorted_set[ind + 1] - elem)
+    except:
+        dif2 = 10 ** 5
+
+    if dif1 > dif2:
+        print(sorted_set[ind + 1])
+    else:
+        print(sorted_set[ind])
 
 
 
