377d1a20e664927b822c2e29dd71d6585f4fcd96
Alex_Ting <adr.tsiklov@mail.ru>
2021-11-26 14:43:18 +0300

    OK

--- Приближённый двоичный поиск.py
+++ Приближённый двоичный поиск.py
@@ -1,8 +1,5 @@
 n, k = input().split()
-arr1 = list(map(int, input().split()))
-set1 = set(arr1)
-sorted_set = sorted(list(set1))
-len_set = len(sorted_set)
+arr1 = sorted(list(set(map(int, input().split()))))
 arr2 = list(map(int, input().split()))
 
 
@@ -10,15 +7,11 @@ def bin_search(arr, elem):
     length = len(arr)
     R, L = length, 0
     while L < R:
-        m = (R + L + 1) // 2
-
-        if m == length:
-            break
-
+        m = (R + L) // 2
         if elem > arr[m]:
-            L = m
+            L = m + 1
         else:
-            R = m - 1
+            R = m
 
     if R == length:
         return R - 1
@@ -26,17 +19,12 @@ def bin_search(arr, elem):
 
 
 for elem in arr2:
-    ind = bin_search(sorted_set, elem)
-    dif1 = abs(sorted_set[ind] - elem)
-    try:
-        dif2 = abs(sorted_set[ind + 1] - elem)
-    except:
-        dif2 = 10 ** 5
+    ind = bin_search(arr1, elem)
 
-    if dif1 > dif2:
-        print(sorted_set[ind + 1])
+    if ind > 0 and abs(arr1[ind - 1] - elem) <= abs(arr1[ind] - elem):
+        print(arr1[ind - 1])
     else:
-        print(sorted_set[ind])
+        print(arr1[ind])
 
 
 
