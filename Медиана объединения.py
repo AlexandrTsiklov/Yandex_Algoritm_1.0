ce565107bf5ce2b2f0f5a9254115d41889406fd2
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-02 09:53:47 +0300

    OK

---
+++ Медиана объединения.py
@@ -0,0 +1,20 @@
+from heapq import merge
+n, l = map(int, input().split())
+lst_pos = [list(map(int, input().split())) for _ in range(n)]
+
+
+def bin_search(l, lst1, lst2):
+    L, R = min(lst1[0], lst2[0]), max(lst1[-1], lst2[-1])
+    merged = list(merge(lst1, lst2))
+    while R != L:
+        m = (R + L + 1) // 2
+        if merged[l - 1] >= m:
+            L = m
+        else:
+            R = m - 1
+    return L
+
+
+for i in range(len(lst_pos)):
+    for j in range(i + 1, len(lst_pos)):
+        print(bin_search(l, lst_pos[i], lst_pos[j]))
