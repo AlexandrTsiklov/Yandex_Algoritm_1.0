d226c54cecbb5faf9c66dfcbf149cf4e341b8efc
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-04 22:31:28 +0300

    OK

---
+++ Рассадка в аудитории.py
@@ -0,0 +1,21 @@
+n, d = map(int, input().split())
+students_list_first = list(map(int, input().split()))
+students_list = sorted(students_list_first)
+
+L, R, max_var, dii = 0, 1, 1, {}
+dii[students_list[0]] = 1
+
+while R < len(students_list_first):
+    if students_list[R] - students_list[L] > d:
+        dii[students_list[R]] = dii[students_list[L]]
+        L += 1
+    else:
+        max_var += 1
+        dii[students_list[R]] = max_var
+    R += 1
+
+print(max_var)
+for i in students_list_first:
+    print(dii[i], end=' ')
+
+
