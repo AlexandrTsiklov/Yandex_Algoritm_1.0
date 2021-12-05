2c5faa8894ed37ea46107288ab7e5c8ab18de6b2
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-05 12:05:17 +0300

    OK

--- Определить вид последовательности.py
+++ Определить вид последовательности.py
@@ -1,61 +1,43 @@
 lst = list()
+count = 0
+flag_CONSTANT = True
+flag_ASCENDING = True
+flag_WEAKLY_ASCENDING = True
+flag_DESCENDING = True
+flag_WEAKLY_DESCENDING = True
 
+before = int(input())
 while True:
-    value = int(input())
-    if value == -2 * 10**9:
+    current = int(input())
+    if current == -2 * 10**9:
         break
-    else:
-        lst.append(value)
+    if current != before:
+        flag_CONSTANT = False
+    if current <= before:
+        flag_ASCENDING = False
+    if current < before:
+        flag_WEAKLY_ASCENDING = False
+    if current >= before:
+        flag_DESCENDING = False
+    if current > before:
+        flag_WEAKLY_DESCENDING = False
 
+    before = current
 
-def CONSTANT(get_list):
-    if len(set(get_list)) == 1:
-        return 'CONSTANT'
-    else:
-        return False
+d = {
+    'CONSTANT': flag_CONSTANT,
+    'ASCENDING ': flag_ASCENDING,
+    'WEAKLY ASCENDING': flag_WEAKLY_ASCENDING,
+    'DESCENDING': flag_DESCENDING,
+    'WEAKLY DESCENDING': flag_WEAKLY_DESCENDING
+}
 
-
-def ASCENDING(get_list):
-    flag = 'ASCENDING'
-    for i in range(1, len(get_list)):
-        if lst[i] <= lst[i - 1]:
-            flag = False
-            break
-    return flag
-
-
-def WEAKLY_ASCENDING(get_list):
-    flag = 'WEAKLY_ASCENDING'
-    for i in range(1, len(get_list)):
-        if lst[i] < lst[i - 1]:
-            flag = False
-            break
-    return flag
-
-
-def DESCENDING (get_list):
-    flag = 'DESCENDING'
-    for i in range(1, len(get_list)):
-        if lst[i] >= lst[i - 1]:
-            flag = False
-            break
-    return flag
-
-
-def WEAKLY_DESCENDING(get_list):
-    flag = 'WEAKLY_DESCENDING'
-    for i in range(1, len(get_list)):
-        if lst[i] > lst[i - 1]:
-            flag = False
-            break
-    return flag
-
-
-final_flag = False
-for i in [CONSTANT(lst), ASCENDING(lst), WEAKLY_ASCENDING(lst), DESCENDING(lst), WEAKLY_DESCENDING(lst)]:
-    if i is not False:
-        final_flag = True
+flag = False
+for i in d:
+    if d[i] is True:
         print(i)
+        flag = True
         break
-if final_flag is False:
-    print('RANDOM')  #
+
+if flag is False:
+    print('RANDOM')
