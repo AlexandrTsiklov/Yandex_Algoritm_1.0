a34468062a4d54c951669055bb7845f0829a258c
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-05 10:27:42 +0300

    OK

--- Точки и отрезки.py
+++ Точки и отрезки.py
@@ -1,15 +1,15 @@
 n, m = map(int, input().split())
 otr_list = [tuple(map(int, input().split())) for _ in range(n)]
-points_list = list(map(int, input().split()))
+points = tuple(map(int, input().split()))
 
 look_at_that, pdict = [], {}
-for point in points_list:
+for point in points:
     pdict[point] = 0
 
 for otr in otr_list:
-    look_at_that.append((otr[0], 0))
-    look_at_that.append((otr[1], 2))
-for point in points_list:
+    look_at_that.append((min(otr), 0))
+    look_at_that.append((max(otr), 2))
+for point in points:
     look_at_that.append((point, 1))
 
 look_at_that.sort()
@@ -23,5 +23,5 @@ for i in range(len(look_at_that)):
     else:
         pdict[look_at_that[i][0]] = i_include_this_count
 
-for point in points_list:
+for point in points:
     print(pdict[point], end=' ')
