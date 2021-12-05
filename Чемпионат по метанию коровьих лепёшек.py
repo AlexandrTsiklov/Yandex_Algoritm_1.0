3a1f7e0080851fd96682baf0c2ad17016ef17ac7
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-05 13:13:54 +0300

    OK

--- Чемпионат по метанию коровьих лепёшек.py
+++ Чемпионат по метанию коровьих лепёшек.py
@@ -1,13 +1,31 @@
-n, dist = int(input()), list(map(int, input().split()))
-max_v, min_v, our_ch = max(dist), min(dist), 0
+n, distances = int(input()), list(map(int, input().split()))
+max_v, min_v, our_ch, position = max(distances), min(distances), 0, 1
 
-for i in range(1, len(dist) - 1):
-    if dist[i - 1] == max_v and dist[i + 1] == min_v and dist[i] % 10 == 5:
-        if dist[i] >= our_ch:
-            our_ch = dist[i]
+flag_out, flag_in = False, False
+for i in range(1, len(distances) - 1):
 
-dist.sort(reverse=True)
-if our_ch == 0:
-    print(our_ch)
-else:
-    print(dist.index(our_ch) + 1) #
+    if distances[i - 1] == max_v:
+        flag_out = True
+
+    if flag_out is True:
+        if distances[i] % 10 == 5 and distances[i + 1] < distances[i]:
+            flag_in = True
+            if distances[i] > our_ch:
+                our_ch = distances[i]
+
+sorted_distances = sorted(distances, reverse=True)
+for dist in sorted_distances:
+    if dist == our_ch:
+        print(position)
+        break
+    position += 1
+
+if flag_in is False:
+    print(0)
+
+
+
+
+
+
+
