27a6e77f45ec3259b9cd50b5204892e8d01e250c
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-07 07:48:09 +0300

    OK

--- Туризм.py
+++ Туризм.py
@@ -1,9 +1,9 @@
 points_list = [tuple(map(int, input().split())) for _ in range(int(input()))]
-roads_list = [tuple(map(int, input().split())) for _ in range(int(input()))]
+roads_list, length = [tuple(map(int, input().split())) for _ in range(int(input()))], len(points_list)
 
 
-def total_height(point_list, start, finish, step, i_plus, what_add):
-    high, my_dict = 0, {1: 0}
+def total_height(point_list, first_elem, start, finish, step, i_plus, what_add):
+    high, my_dict = 0, {first_elem: 0}
     for i in range(start, finish, step):
         if point_list[i][1] > point_list[i + i_plus][1]:
             high += point_list[i][1] - point_list[i + i_plus][1]
@@ -11,12 +11,12 @@ def total_height(point_list, start, finish, step, i_plus, what_add):
     return my_dict
 
 
-points_dict_ahead = total_height(points_list, 1, len(points_list), 1, -1, 1)
-points_dict_back = total_height(points_list, -2, -len(points_list) - 1, -1, 1, 0)
+points_dict_ahead = total_height(points_list, 1, 1, length, 1, -1, 1)
+points_dict_back = total_height(points_list, length, -2, -length - 1, -1, 1, length + 1)
 
 for road in roads_list:
     if road[0] < road[1]:
         print(points_dict_ahead[road[1]] - points_dict_ahead[road[0]])
     else:
-        print(points_dict_back[road[0]] - points_dict_back[road[1]])#
+        print(points_dict_back[road[1]] - points_dict_back[road[0]])
 
