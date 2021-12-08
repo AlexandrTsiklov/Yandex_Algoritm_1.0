52a860af618efb701a34e4b24129841408d2d65c
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-08 16:10:09 +0300

    TL 13 тест

--- Расшифровка письменности Майя.py
+++ Расшифровка письменности Майя.py
@@ -5,16 +5,18 @@ for char in w:
     if char not in dict_example:
         dict_example[char] = 0
     dict_example[char] += 1
-    dict_our[char] = 0
-we_were_there, ness_summa = None, len(dict_example)
+    dict_our[char] = [0, False]
+ness_summa = len(dict_example)
 
 for i in range(0, g):
     char_r = s[i]
     if char_r in dict_our:
-        dict_our[char_r] += 1
-        if dict_our[char_r] == dict_example[char_r]:
+        dict_our[char_r][0] += 1
+        if dict_our[char_r][0] == dict_example[char_r]:
+            dict_our[char_r][1] = True
             summa += 1
-        elif dict_our[char_r] > dict_example[char_r]:
+        elif dict_our[char_r][1] is True:
+            dict_our[char_r][1] = False
             summa -= 1
 if summa == ness_summa:
     total += 1
@@ -22,29 +24,32 @@ if summa == ness_summa:
 for i in range(g, s_l):
     char_r = s[i]
     char_l = s[i - g]
-
     if char_l in dict_our:
-        dict_our[char_l] -= 1
-        if dict_our[char_l] == dict_example[char_l]:
+        dict_our[char_l][0] -= 1
+        if dict_our[char_l][0] == dict_example[char_l]:
+            dict_our[char_l][1] = True
             summa += 1
-        elif dict_our[char_l] == dict_example[char_l] - 1:
+        elif dict_our[char_l][1] is True:
+            dict_our[char_l][1] = False
             summa -= 1
 
     if char_r in dict_our:
-        dict_our[char_r] += 1
-        if dict_our[char_r] == dict_example[char_r]:
+        dict_our[char_r][0] += 1
+        if dict_our[char_r][0] == dict_example[char_r]:
+            dict_our[char_r][1] = True
             summa += 1
-            we_were_there = True
-        elif dict_our[char_r] > dict_example[char_r] and we_were_there:
+        elif dict_our[char_r][1] is True:
+            dict_our[char_r][1] = False
             summa -= 1
-            we_were_there = False
 
-    if summa == ness_summa:
-        total += 1
+        if summa == ness_summa:
+            total += 1
 
 print(total)
 
-
+# 2 6
+# wQ
+# wQQwwQ
 
 
 
