b6d8af88a401eb228286e20a57dc01a336679d37
Alex_Ting <adr.tsiklov@mail.ru>
2021-12-08 16:48:34 +0300

    OK

--- Расшифровка письменности Майя.py
+++ Расшифровка письменности Майя.py
@@ -1,6 +1,8 @@
 g, s_l = map(int, input().split())
 w, s, dict_example, dict_our, total, summa = input(), input(), {}, {}, 0, 0
 
+# ВНИМАНИЕ !!!! При тестировании выбираем язык Python 3.7 (PyPy)
+
 for char in w:
     if char not in dict_example:
         dict_example[char] = 0
@@ -8,30 +10,18 @@ for char in w:
     dict_our[char] = [0, False]
 ness_summa = len(dict_example)
 
-for i in range(0, g):
+for i in range(0, s_l):
     char_r = s[i]
-    if char_r in dict_our:
-        dict_our[char_r][0] += 1
-        if dict_our[char_r][0] == dict_example[char_r]:
-            dict_our[char_r][1] = True
-            summa += 1
-        elif dict_our[char_r][1] is True:
-            dict_our[char_r][1] = False
-            summa -= 1
-if summa == ness_summa:
-    total += 1
-
-for i in range(g, s_l):
-    char_r = s[i]
-    char_l = s[i - g]
-    if char_l in dict_our:
-        dict_our[char_l][0] -= 1
-        if dict_our[char_l][0] == dict_example[char_l]:
-            dict_our[char_l][1] = True
-            summa += 1
-        elif dict_our[char_l][1] is True:
-            dict_our[char_l][1] = False
-            summa -= 1
+    if i >= g:
+        char_l = s[i - g]
+        if char_l in dict_our:
+            dict_our[char_l][0] -= 1
+            if dict_our[char_l][0] == dict_example[char_l]:
+                dict_our[char_l][1] = True
+                summa += 1
+            elif dict_our[char_l][1] is True:
+                dict_our[char_l][1] = False
+                summa -= 1
 
     if char_r in dict_our:
         dict_our[char_r][0] += 1
@@ -46,20 +36,3 @@ for i in range(g, s_l):
             total += 1
 
 print(total)
-
-# 2 6
-# wQ
-# wQQwwQ
-
-
-
-
-
-
-
-
-
-
-
-
-
