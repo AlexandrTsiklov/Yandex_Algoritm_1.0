9cf8e6ea53f34e91de8cdf5775498e34b869146a
Alexandr Tsiklov <53411044+AlexandrTsiklov@users.noreply.github.com>
2023-03-26 09:40:13 +0300

    OK

--- Охрана.py
+++ Охрана.py
@@ -1,5 +1,4 @@
-k, tests = int(input()), []  # статус ОК на интерпретаторе Python 3.11.2, Python 3.9 (PyPy 7.3.11) выдавал ML
-for _ in range(k):
+for _ in range(int(input())): # статус ОК на интерпретаторе Python 3.11.2, Python 3.9 (PyPy 7.3.11) выдавал ML
     dataline = input().split()
     n, events = int(dataline[0]), []
     for i in range(1, 2 * n, 2):
@@ -11,8 +10,7 @@ for _ in range(k):
     if n == 1:
         print("Accepted" if events[0][0] == 0 and events[0][1] == 10000 else "Wrong Answer")
     for i in range(1, n):
-        prevguard = events[i - 1]
-        currguard = events[i]
+        currguard, prevguard = events[i], events[i - 1]
         if (i == 1 and prevguard[0] != 0) or (prevguard[0] == currguard[0]) or (prevguard[1] < currguard[0]) or (prevguard[1] >= currguard[1]) or (i == n - 1 and currguard[1] != 10000) or (i > 1 and events[i - 2][1] >= currguard[0]):
             print("Wrong Answer")
             break
